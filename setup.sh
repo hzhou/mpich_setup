4fcdeb51c4a5449cc4d9a81d3b3ec76dc8817a00
Hui Zhou <hzhou@thetalogin4.tmi.alcf.anl.gov>
2020-08-05 18:41:41 +0000

    update
    color prompt
    mpich_pr master -> main

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ if test $0 != "mpich_setup/setup.sh" ; then
 	exit 1;
 fi
 
+mkdir -p bin
 cp mpich_setup/_bashrc .bashrc
 cp mpich_setup/_vimrc  .vimrc
+cp mpich_setup/get_mpich_pr.pl bin/
 
 git clone https://github.com/pmodels/mpich mpich-github
 git clone https://github.com/hzhou/mymake_mpich mymake
@@ -15,8 +17,10 @@ git clone https://github.com/hzhou/MyDef MyDef
 
 cd modules
 bash build_hwloc.sh
-bash build_jsonc.sh
-MODULES="hwloc json-c" bash build_tarball.sh
+bash build_json-c.sh
+bash build_yaksa.sh
+bash build_izem.sh
+MODULES="hwloc json-c yaksa izem" bash build_tarball.sh
 mv modules.tar.gz ..
 cd ..
 
