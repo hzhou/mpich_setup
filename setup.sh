0171d1b5b5219318be44a305f4148ce1bf69d69e
Hui Zhou <hzhou321@anl.gov>
2021-11-22 17:50:51 -0600

    update

--- setup.sh
+++ setup.sh
@@ -10,18 +10,20 @@ cp mpich_setup/_bashrc .bashrc
 cp mpich_setup/_vimrc  .vimrc
 cp mpich_setup/get_mpich_pr.pl bin/
 
-git clone https://github.com/pmodels/mpich mpich-github
-git clone https://github.com/hzhou/mymake_mpich mymake
-git clone https://github.com/hzhou/mpich-modules modules
-git clone https://github.com/hzhou/MyDef MyDef
+if test ! -d MyDef; then
+    git clone https://github.com/pmodels/mpich mpich-github
+    git clone https://github.com/hzhou/mymake_mpich mymake
+    git clone https://github.com/hzhou/mpich-modules modules
+    git clone https://github.com/hzhou/MyDef MyDef
+fi
 
-cd modules
-bash build_hwloc.sh
-bash build_json-c.sh
-bash build_yaksa.sh
-bash build_izem.sh
-MODULES="hwloc json-c yaksa izem" bash build_tarball.sh
-mv modules.tar.gz ..
-cd ..
-
-
+if false ; then
+    cd modules
+    bash build_hwloc.sh
+    bash build_json-c.sh
+    bash build_yaksa.sh
+    bash build_izem.sh
+    MODULES="hwloc json-c yaksa izem" bash build_tarball.sh
+    mv modules.tar.gz ..
+    cd ..
+fi
